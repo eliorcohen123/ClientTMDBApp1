e5f88ae660c448af5cab3d20145e57228248f3ec
EliorCohen <elior123456123123@gmail.com>
2020-11-15 14:15:46 +0200

    update

--- app/src/main/java/com/example/clienttmdbapp/MainActivity.java
+++ app/src/main/java/com/example/clienttmdbapp/MainActivity.java
@@ -2,15 +2,11 @@ package com.example.clienttmdbapp;
 
 import android.database.Cursor;
 import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.ListView;
 import android.widget.TextView;
-
-import java.util.Collections;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
